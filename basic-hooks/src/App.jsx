d28614d97a295024ee91dbfe8f46989b9b2a28c8
Jpduker <jayaprakashnis619@gmail.com>
2023-03-16 21:22:16 +0530

    added increment count method

--- basic-hooks/src/App.jsx
+++ basic-hooks/src/App.jsx
@@ -7,15 +7,21 @@ function App() {
   const [count, setCount] =useState(4)
 
   function decrementCount() {
+    setCount(count -1 )
     
+    
+  }
+
+  function  incrementCount() {
+    setCount(count +1)
     
   }
   return (
    <>
 
-      <button onClick={decrementCount} >+</button>
+      <button onClick={incrementCount} >+</button>
       <span>{count}</span>
-      <button type="">-</button>
+      <button onClick={decrementCount}>-</button>
     </>
   )
 }
